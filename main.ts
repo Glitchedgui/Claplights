2ca0602c52514aa2aac82957f807f220aa189d72
Glitchedgui <82472644+Glitchedgui@users.noreply.github.com>
2021-10-08 13:23:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let LightsOn = false
+input.onSound(DetectedSound.Loud, function () {
+    LightsOn = !(LightsOn)
+    if (LightsOn) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
